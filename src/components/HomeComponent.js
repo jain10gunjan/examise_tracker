4a15b2f43a33232f169b0b357ac4dc0dbd463d14
Gunjan Jain <jain10gunjan@gmail.com>
2024-09-27 13:24:10 +0530

    D

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -70,8 +70,40 @@ const HomeComponent = () => {
   return (
     <>
       <Navbar />
-
-      <section>
+      <div class="mt-16 mb-24 max-w-4xl flex flex-col mx-auto size-full">
+        <main id="content">
+          <div class="text-center py-10 px-4 sm:px-6 lg:px-8">
+            <h1 class="block text-7xl font-bold text-gray-800 sm:text-9xl">
+              404
+            </h1>
+            <p class="mt-3 text-gray-600">We are Under Maintenance</p>
+            <p class="text-gray-600">Sorry, For Any Inconveniance.</p>
+            <div class="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+              <a
+                class="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                href="/"
+              >
+                <svg
+                  class="shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m15 18-6-6 6-6" />
+                </svg>
+                Back to Home
+              </a>
+            </div>
+          </div>
+        </main>
+      </div>
+      {/* <section>
         <div className="rain-container">
           {Array.from({ length: 20 }).map((_, index) => (
             <div key={index} className="raindrop" />
@@ -407,7 +439,7 @@ const HomeComponent = () => {
           </div>
         </div>
         <Footer />
-      </section>
+      </section> */}
     </>
   );
 };
