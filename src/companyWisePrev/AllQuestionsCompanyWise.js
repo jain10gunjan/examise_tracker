d9a5eda7c3892faa2bd0f3482f403e1e97f9bfe2
Gunjan Jain <jain10gunjan@gmail.com>
2024-09-29 10:08:11 +0530

    Fix.

--- src/companyWisePrev/AllQuestionsCompanyWise.js
+++ src/companyWisePrev/AllQuestionsCompanyWise.js
@@ -18,6 +18,10 @@ const AllQuestionCompanyWise = () => {
   const tabs = ["Numerical-Ability", "Verbal-Ability", "Reasoning-Ability"];
   const defaultTab = "Numerical-Ability";
   const [activeTab, setActiveTab] = useState(defaultTab);
+  const [isVisible, setIsVisible] = useState(true);
+  const dismissAlert = () => {
+    setIsVisible(false);
+  };
 
   useEffect(() => {
     // Load user data from local storage
@@ -188,6 +192,69 @@ const AllQuestionCompanyWise = () => {
         <section className="mt-20 text-gray-600 body-font">
           <div className="container mx-auto flex flex-col px-5 py-10 justify-center items-center">
             <div className="w-full md:w-2/3 flex flex-col mb-16">
+              {isVisible ? (
+                <div
+                  id="alert-additional-content-1"
+                  className="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+                  role="alert"
+                >
+                  <div className="flex items-center">
+                    <svg
+                      className="flex-shrink-0 w-4 h-4 me-2"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                    </svg>
+                    <span className="sr-only">Info</span>
+                    <span className="text-sm font-medium">
+                      Important Disclaimer
+                    </span>
+                  </div>
+                  <div className="mt-2 mb-4 text-sm">
+                    <ul className="mt-1.5 list-disc list-inside">
+                      <li>
+                        We have collected these questions from sources like the
+                        Internet and social media.
+                      </li>
+                      <li>
+                        We have not generated these questions. We have only
+                        gathered and summarized the questions that are already
+                        available on the Internet in one place.
+                      </li>
+                      <li>
+                        We are not liable for any misprints, misinformation, or
+                        other logical errors, and we do not hold any copyright
+                        infringement.
+                      </li>
+                      <li>
+                        If you want to remove any questions, please contact us
+                        with proper documentation. We will respond within 48
+                        hours.
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div className="flex">
+                    {/* <button type="button" className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <svg className="me-2 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
+            <path d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+          </svg>
+          View more
+        </button> */}
+                    <button
+                      type="button"
+                      className="text-blue-800 bg-transparent border border-blue-800 hover:bg-blue-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-blue-600 dark:border-blue-600 dark:text-blue-400 dark:hover:text-white dark:focus:ring-blue-800"
+                      onClick={dismissAlert}
+                      aria-label="Close"
+                    >
+                      Dismiss
+                    </button>
+                  </div>
+                </div>
+              ) : null}
               <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-gray-900">
                 TCS NQT Previous Year{" "}
                 <p className="mt-2 text-sm text-gray-600">
